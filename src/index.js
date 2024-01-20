7dd1d80563c09f8296b034147e6bcb00c2ed3508
sagonzalez76 <sagonzalez76@misena.edu.co>
2024-01-20 15:33:13 -0500

    truncate

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { sequelize } from "./database/database.js";
 
 async function main() {
 
-  await sequelize.sync({ force: false });
+  await sequelize.sync({ force: true });
 
   app.listen(process.env.PORT);
   console.log(`Server on port ${process.env.PORT}`);
